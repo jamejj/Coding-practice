4b613ceebae5d5e04c2926010dd70ebdaa0c063b
jamejj <jjaworskyy@gmail.com>
2026-04-14 01:09:04 +0200

    HackerRank

---
+++ HackerRank/Python(Basic)/Strings/String Validators.py
@@ -0,0 +1,36 @@
+if __name__ == '__main__':
+    s = input()
+
+    isAlphaNumeric = False
+    isAlpha = False
+    isDigit = False
+    isLower = False
+    isUpper = False
+
+    for i in range(len(s)):
+        if s[i].isalnum():
+           isAlphaNumeric = True
+
+        if s[i].isalpha():
+            isAlpha = True
+
+        if s[i].isdigit():
+            isDigit = True
+
+        if s[i].islower():
+            isLower = True
+
+        if s[i].isupper():
+            isUpper = True
+
+
+
+    print(isAlphaNumeric)
+    print(isAlpha)
+    print(isDigit)
+    print(isLower)
+    print(isUpper)
+
+
+
+
